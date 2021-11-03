a49b8ed9a332269c2a0dbf2d1d6a70b9af5413c0
eqsoftpm <eqsoftpm@gmail.com>
2021-11-03 23:14:23 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
-  imports: [
+  imports: [  
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  customerInfo!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.customerInfo = this.fb.group({
+      name: [''],
+      phone: [''],
+      email: [''],
+      address: ['']
+    })
+  }
+
+  resetForm(): void {
+    // this.customerInfo.get('name')?.setValue('');
+    // this.customerInfo.get('phone')?.setValue('');
+    // this.customerInfo.get('email')?.setValue('');
+    // this.customerInfo.get('address')?.setValue('');
+    this.customerInfo.reset();
+  }
+
+  submitForm(): void {
+        
+  }
+
+
+}
